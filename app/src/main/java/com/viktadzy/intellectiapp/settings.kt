32e4ffa06ee2cd3ea93fb329e819cfb3ec693e35
VIKTA DZY <viktamuhumuza@gmail.com>
2022-06-27 12:23:59 +0300

    initial commit

--- app/src/main/java/com/viktadzy/intellectiapp/settings.kt
+++ app/src/main/java/com/viktadzy/intellectiapp/settings.kt
@@ -3,7 +3,7 @@ package com.viktadzy.intellectiapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SettingsActivity : AppCompatActivity() {
+class settings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
